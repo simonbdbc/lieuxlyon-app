e4072c55ff1ab924443d11111c094132176de1b7
simonbdbc <simonbdbc@gmail.com>
2019-06-12 16:14:15 +0200

    update service worker

--- sw.js
+++ sw.js
@@ -25,6 +25,7 @@ var filesToCache = [
 // Listen to installation event
 self.addEventListener("install", function(e) {
   //console.log("[ServiceWorker] Install");
+  self.skipWaiting();
   e.waitUntil(
     caches.open(shellCacheName).then(function(cache) {
       console.log("[ServiceWorker] Caching app shell");
